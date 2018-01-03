d4611b87cf206858b590d7c53ea9a649ef770cd1
lbildzinkas <l.barletta@gmail.com>
2018-01-03 23:51:11 +0100

    Adjustments after review

--- src/Book.jsx
+++ src/Book.jsx
@@ -20,7 +20,7 @@ class Book extends Component {
           return (
             <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                            { book.imageLinks !== undefined && <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>}
                             <div className="book-shelf-changer">
                               <select value={this.state.value} onChange={this.handleChange}>
                                 <option disabled>Move to...</option>
